170828e5e83f25213b27f823410f4ebf05c558b8
Davi Marques <davim.costa@outlook.com>
2022-10-05 21:41:41 -0300

    [UPDATE] Implementação das funções de Disciplina

--- app/Controllers/Http/DisciplinasController.ts
+++ app/Controllers/Http/DisciplinasController.ts
@@ -5,6 +5,10 @@ import Disciplina from "App/Models/Disciplina";
 export default class DisciplinasController {
 
     index(){
+
+
+
+        
         return Disciplina.all()
     }
 
@@ -13,6 +17,31 @@ export default class DisciplinasController {
         return await Disciplina.create(dados)
     }
 
+    show({request}) {
+        const id = request.param('id')
+
+        return Disciplina.findOrFail(id)
+    }
+
+    async destroy({request}) {
+        const id = request.param('id')
+        const disciplina = await Disciplina.findOrFail(id)
+
+        return disciplina.delete()
+
+    }
+
+   async update({request}) {
+        const id = request.param('id')
+        const disciplina = await Disciplina.findOrFail(id)
+
+        const dados = request.only(['nome', 'curso_id'])
+
+        disciplina.merge(dados)
+
+        return disciplina.save()
+
+    }
 
 }
 
